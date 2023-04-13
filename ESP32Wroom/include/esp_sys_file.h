3ee0a41cc2a4a11fbccf3d03be0c2c0bc5dbe69f
hwmat <1293893128@qq.com>
2023-04-13 16:24:30 +0800

    feat:add the spiffs
    1.save file the spiffs
    
    no fix.

---
+++ ESP32Wroom/include/esp_sys_file.h
@@ -0,0 +1,13 @@
+#ifndef ESP_SYS_FILE_H
+#define ESP_SYS_FILE_H
+
+/**
+ * @brief 参考网页 https://blog.csdn.net/Naisu_kun/article/details/88567154
+ *        
+ * 
+ */
+
+int esp_spiffs_setup(void);
+int esp_spiffs_task(void);
+
+#endif // ESP_SYS_FILE_H
